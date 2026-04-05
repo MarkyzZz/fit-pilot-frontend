9eef7c58f25ff06b575603ae881eb088b5739ff3
Marin Cunup <25markyzzz@gmail.com>
2026-04-05 22:42:05 +0300

    #5 Refactor `app.config.ts` for consistent formatting
    - Adjust import statements for compact structure.
    - Fix indentation and formatting in providers array.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,8 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
-import {
-  provideHttpClient,
-  withInterceptors,
-  withXsrfConfiguration,
-} from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
 import { sanctumInterceptor } from './interceptors/sanctum.interceptor';
 import { routes } from './app.routes';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideTranslateService } from '@ngx-translate/core';
@@ -32,36 +20,36 @@ import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      }),
-      withComponentInputBinding()
-    ),
-    provideHttpClient(
-      withInterceptors([sanctumInterceptor]),
-      withXsrfConfiguration({
-        cookieName: 'XSRF-TOKEN',
-        headerName: 'X-XSRF-TOKEN',
-      })
-    ),
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    importProvidersFrom(
-      FormsModule,
-      ReactiveFormsModule,
-      MaterialModule,
-      TablerIconsModule.pick(TablerIcons),
-      NgScrollbarModule
-    ),
-    provideTranslateService(),
-    provideTranslateHttpLoader({
-      prefix: './assets/i18n/',
-      suffix: '.json'
-    }),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(
+            routes,
+            withInMemoryScrolling({
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+            }),
+            withComponentInputBinding(),
+        ),
+        provideHttpClient(
+            withInterceptors([sanctumInterceptor]),
+            withXsrfConfiguration({
+                cookieName: 'XSRF-TOKEN',
+                headerName: 'X-XSRF-TOKEN',
+            }),
+        ),
+        provideClientHydration(),
+        provideAnimationsAsync(),
+        importProvidersFrom(
+            FormsModule,
+            ReactiveFormsModule,
+            MaterialModule,
+            TablerIconsModule.pick(TablerIcons),
+            NgScrollbarModule,
+        ),
+        provideTranslateService(),
+        provideTranslateHttpLoader({
+            prefix: './assets/i18n/',
+            suffix: '.json',
+        }),
+    ],
 };
