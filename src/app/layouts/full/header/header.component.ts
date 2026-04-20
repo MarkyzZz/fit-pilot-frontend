6d2a53b1fa2ffc17d6cc460ea24f0930514b75d2
Marin Cunup <25markyzzz@gmail.com>
2026-04-21 00:11:24 +0300

    #26 Fix logout functionality and improve header component
    - Replace `tap` with `finalize` in `AuthService.logout()` to ensure navigation to the login page.
    - Update `header.component.html` to use a button for the logout action.
    - Refactor `HeaderComponent` to inject `AuthService` and handle logout logic.

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,39 +1,34 @@
-import {
-  Component,
-  Output,
-  EventEmitter,
-  Input,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, ViewEncapsulation } from '@angular/core';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { AppSettings } from 'src/app/config';
+import { AuthService } from '../../../services/auth.service';
+import { finalize } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
-  imports: [
-    RouterModule,
-    NgScrollbarModule,
-    TablerIconsModule,
-    MaterialModule
-  ],
-  templateUrl: './header.component.html',
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-header',
+    imports: [RouterModule, NgScrollbarModule, TablerIconsModule, MaterialModule],
+    templateUrl: './header.component.html',
+    encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
-  @Input() showToggle = true;
-  @Input() toggleChecked = false;
-  @Output() toggleMobileNav = new EventEmitter<void>();
-  @Output() toggleMobileFilterNav = new EventEmitter<void>();
-  @Output() toggleCollapsed = new EventEmitter<void>();
+    @Input() showToggle = true;
+    @Input() toggleChecked = false;
+    @Output() toggleMobileNav = new EventEmitter<void>();
+    @Output() toggleMobileFilterNav = new EventEmitter<void>();
+    @Output() toggleCollapsed = new EventEmitter<void>();
+    showFiller = false;
+    @Output() optionsChange = new EventEmitter<AppSettings>();
+    private readonly authService = inject(AuthService);
+    private readonly router = inject(Router);
 
-  showFiller = false;
-
-
-  @Output() optionsChange = new EventEmitter<AppSettings>();
-
-
+    public logout(): void {
+        this.authService
+            .logout()
+            .pipe(finalize(() => this.router.navigate(['/authentication/login'])))
+            .subscribe();
+    }
 }
